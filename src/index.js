16cbb3eda0cc29bf39318705a9cf84a127498ff9
vladklen <Vlad6097944@gmail.com>
2023-01-22 20:12:50 +0300

    Test passed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let result = [];
+    if (matrix) {
+        for (let index = 0; index < matrix.length; index++) {
+            if (index % 2 == 0) {
+                console.log(index);
+                matrix[index].map((el) => result.push(el));
+            } else {
+                matrix[index].reverse().map((el) => result.push(el));
+            }
+        }
+    }
+    return result;
+};
